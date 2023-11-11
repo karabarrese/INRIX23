325b8ec0fcfebff5c4aca99b0baaf22d487379c3
PragnyaVijayan <pragnya.vijayan@gmail.com>
2023-11-11 15:31:11 -0800

    Pushing new Firebase templates

--- Insight/App.tsx
+++ Insight/App.tsx
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Homescreen reached</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {

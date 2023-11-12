516c427113cba6a240409b4ccd605a4f4c31d6c5
mfollosco <mvfollosco@gmail.com>
2023-11-11 17:56:35 -0800

    map w/hard coded long,lat

--- Insight/App.js
+++ Insight/App.js
@@ -1,37 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    console.log("useEffect")
-    fetch("http://172.31.181.212:5001/").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-        console.log(data.message)
-      }
-    )
-
-    // console.log(res)
-  }, [])
-
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
-      {(typeof data.message === 'undefined') ? (
-        <Text>Loading...</Text>
-      ) : (
-        <Text>{data.message}</Text>
-        // data.message.map((message, i) => (
-        //   <Text key={i}>{message}</Text>
-        // ))
-      )}
-      <StatusBar style="auto" />
+      <MapView 
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      >
+        <Marker coordinate={{
+          latitude: 37.78825,
+          longitude: -122.4324
+        }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -39,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });

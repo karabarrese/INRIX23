d157f548d51c95d3f67994fbf05622f04bb5b8ec
rose413 <irenechang949@gmail.com>
2023-11-12 03:59:08 -0800

    Update App.js

--- Insight/App.js
+++ Insight/App.js
@@ -1,20 +1,28 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import LocationPage from './app/screens/LocationPage';
+import DrivingFactPage from './app/screens/DrivingFactPage';
+import ContactsScreen from './app/screens/ContactsPage';
+// import { NavigationContainer } from '@react-navigration/native';
+// import { createNativeStackNavigator} from '@react-navigration/native-stack';
+ 
+//App Component
+export default function App() { 
+  const handlePress = () => console.log("Text pressed")
+  
+  /*return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="WelcomeScreen">
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="LoginScLoreen" component={LoginScreen} />
+        <Stack.Screen name="FactHome" component={FactHomeScreen} />
+        <Stack.Screen name="Contacts" component={ContactsScreen} />
+        <Stack.Screen name="LocationPg" component={LocationPgScreen} />
+        <Stack.Screen name="Driving" component={DrivingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  */
+};

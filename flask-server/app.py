b42e0769f3da19b7621adee2dd6b4cc121c1eba3
TiffanyN22 <tnguyen22@ndsj.org>
2023-11-11 16:45:02 -0800

    Get just description and location data from Incidents API

--- flask-server/app.py
+++ flask-server/app.py
@@ -13,19 +13,22 @@ def index():
 
   payload = {}
   headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjZpeWIwZGYxdGoiLCJ0b2tlbiI6eyJpdiI6IjcxMDU4ZjQ2YjhhZDNiZGJhM2U0NDAxYTU4NjY5MTFjIiwiY29udGVudCI6IjMxOGY1MGMxYTAzNjg5Yjc3ZGRiYWNmZTcyYTBlYzdlMGZkZTc3M2FmMjRlNjMxMGFiM2MwNjQxZGM0MjY3N2RjMDkzZjY1MTFhMTNjMWRkOTdlNTY1ZjRmZGNmNTI0MzY3Zjc1YmUzYmY3OWUxOWNlM2U1NjZkMGZhNWFmYWM0MDliYzMyNTEwNzZjNmMwNzQyNWYzODI0YmM5ZDI2Y2E0ZjNiZDhhNDRmZmZhMWNhMzg0ZjkyZGQ1YTk1YTMwZTY1ZTQwZjQ2YmNhOGVmMzE5NjdiOTkzNjJiYjgwYzhiOGY2OTJhMjI1YWI0Nzc2NTQyMWYyZGEyZGRiNWJkNzAzOGI3ZmM0MTdhZTRiYjIxZjRjMTk2NmExNzlkODU4ODdiZjY3NjlkZjEwN2NmZTRjYWY0ZjVlOWE4ODg1MzcxM2I4OWU1ZmJmYzhiZjUwNTY3YzBjOWEzNDFiOGU1MmRiYTM0NDk5ODFlODA3MjMwZjNlMTFjYmFmZTZjZDM0NjQwOWY5Nzg2NmQwOGM4OTdkYjI2YzAxZTExNzAwNWMwNTkxMmYxNDM3NjEwOTJiMjNlMGIwNzljMDRmYzI0MGViMmJhYWY3ZDQyYmI1NmEwNTU3MWY4ZmZmNWQ5MGQ1YzRhMjVmOTY3NGU4NmM0ODQ4ZDRlMTFjYmU1OWUwNmFjZTcwNzU2YjcxZjM4YTUzNDg1N2Q5MTdhN2MxODBjMjUwYTZmNDFjZGJmY2ZhMDM5MjZjMjk1NjZhMTA5ZWNkZTMxZDVlYTg3NTIyNTNlZWU0MjVmMTRjZjYyNDU4NWQzMGEwODI4ZjAyNTFhMTFjMzI4MTgxZDJjYjE2ZjQxNDY4MDBmZWI1ODkxZDMzNGU2NTk4ZmFlIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI3MTA1OGY0NmI4YWQzYmRiYTNlNDQwMWE1ODY2OTExYyIsImNvbnRlbnQiOiIyMWM0NDhkZjkxM2VhNGVmMDNmYzgwYzM0NWI5ODgwODMxZjM1ZjI4ZWE2YzA0NjNmMTRlMjAzOWYwNmM2ZDQ3ZDBiOWVjNWI2NjFlZDZlNzlkZTExOWNhIn0sImp0aSI6IjQ4ZTczYmM3LTRjOTctNDBlNS1hMmY3LTc3MTg5YzAxYTJkNCIsImlhdCI6MTY5OTczMTIwMCwiZXhwIjoxNjk5NzM0ODAwfQ.B2L4GRkT4yoc_9QZ4-Jhfm99De_Oi_CY2T2VjlE_OBA","expiry":"2023-11-11T20:33:20.244Z',
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjZpeWIwZGYxdGoiLCJ0b2tlbiI6eyJpdiI6IjVmNjhiNzhlNGRhNWZhY2JiYmE2NjUxYWZkNzFhZjgzIiwiY29udGVudCI6ImFhNTNmYjBkZjBjMGIwNzI1ZTQ1MTM0MTVkMzE1MzgzZDAzZTM1MzRlNTA1N2Y1N2Q5MGQ0N2YxNGE1NjhmYThlOWJmODQxMmFkM2NkZDQyOTY3MzA2ZWVmMWRjNDZlOWI5NGQ1ZjRhM2QzMjNjODQ1NjZmNWEzNzEzMDlmNGJlNzZmYWI1ZjI1ZjdlY2ZmYjY0YjBjMGNiOTNjNzFlZTk2NDZiYTc5NTJhZjY5MzRjZTIwZDVlM2QwNDFkZDMwNWY5Mzc1NDUzMDAzNDhiNmVmNmIwN2EzOWM3NGY2MDM0NzVlNDIxZDEyYjZmMmNiZDVlMTczZDI5YTVlNGNhYmUyNmFiZmEyMTFlNDFhMjE5MmM5NTg0MzhmZGFkMGQ4ZDk2YzI5MmRhNGM2NzM2YWNjNzJmZGEyMmU1ZWEzMzI2ZjM2MTY3YTI0MDQ1NDVkZjhiYjRmZmU0OWUyNDkzZDVlZTUyNGFhMDYwZGVlMmJlNzM0OWU2ZjE3ODI1Yjc0MGU0MTE4MDY0OWRhZjJlNDZlNmJkMjhkOWU2NjhhZWUyY2FmNjI5M2MxMDRlNmRkZmQ4YTE1NTkwMGExNmI4NTEyNzc4Y2QxZWVhMzYxNTQzY2NjOWM1ODAyODY1NjkxODUzOGJlODI5YWNmOTljMDg1YTQ2MDQ2NTc0ZTNmNTVjNzc4NzRjNjU2NjU4YWU0M2ExNzkyY2VlZjYyODMzODRkZDJiMGEyMWM4MTJjNjI4MmQ4NWI3YTMzM2M3M2Q5YjFiNjljYjNjN2NmOTM1ZjU5NGY5YWUyOTlmMGVjY2ViYTcyZDM0YzAzYzU5ZWRlYTQxM2I3YzY0NjYwYjBkMDdlMDM2ODVhNTgyMjg0MTU5YWU1ZDVhIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI1ZjY4Yjc4ZTRkYTVmYWNiYmJhNjY1MWFmZDcxYWY4MyIsImNvbnRlbnQiOiJhMTc5Y2IzN2U0ZDdhNDRiNDk0OTFkNTI2ZjdhNmFkZmU0MjQzZDAyZmY1YjFkMDVkODBhNDBmZjZkNGNhNWM1ZDM5NmZkNjdjYzNmYzIzOGJjNDM3NmQwIn0sImp0aSI6IjcyNTZlZDk3LTZiMTMtNDBkYi1hMjYwLTYxZjM4MjAwZTk0ZSIsImlhdCI6MTY5OTc0ODU5MCwiZXhwIjoxNjk5NzUyMTkwfQ.RFDuUFhXajvetlG-k-ICTyWTbwI-DpxNGES81exi7jk',
     'Cookie': 'lang=en-US'
   }
 
   response = requests.request("GET", url, headers=headers, data=payload)
+  incidents = json.loads(response.text)["result"]["incidents"]
 
-  # print(response.text)
-  # return response.text
-  # return jsonify(response.text)
-  # return jsonify({'name': 'alice2',
-  #                 'email': 'alice@outlook.com'})
-  # return{"members": ["Member1", "Member2", "Member3"]}
-  return{"message":"tiffany will be happy if you show up pls"}
+  my_dict = {"incidents": []}
+  my_json = json.dumps(my_dict)
+  incidents_json = json.loads(my_json)
+
+  for i in incidents:
+    # data_array.append([incidents[0]["geometry"]["coordinates"],i["descriptions"][0]["desc"]])
+    newJson = {"location": i["geometry"]["coordinates"],"description": i["descriptions"][0]["desc"]}
+    incidents_json["incidents"].append(newJson)
+  return incidents_json
 
 if __name__ == "main":
   app.run(debug=True, host="0.0.0.0", port=5001)

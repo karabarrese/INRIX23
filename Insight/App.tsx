fddc28beaeddbf15c417e6a83de3a59977f9b647
Kara Barrese <karabarrese@gmail.com>
2023-11-12 04:52:58 -0800

    Revert "update"
    This reverts commit 0592cd2f476e93a3cfddc2f171fb85ca5e992054.

--- Insight/App.tsx
+++ Insight/App.tsx
@@ -1,42 +1,20 @@
-import SignupScreen from './SignupScreen';
-import docId from './SignupScreen';
-import ContactsScreen from './ContactsScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Homescreen reached</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+} 
 
-const Stack = createNativeStackNavigator();
-
-// const AppNavigator = () => (
-//   <NavigationContainer>
-//     <Stack.Navigator>
-//       <Stack.Screen name="Signup" component={SignupScreen} />
-//       <Stack.Screen name="Contacts" component={ContactsScreen} />
-//     </Stack.Navigator>
-//   </NavigationContainer>
-// );
-
-
-class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='SignUp'>
-          <Stack.Screen
-            name="SignUp"
-            component={SignupScreen} 
-          />
-          <Stack.Screen
-            name="Contacts"
-            component={ContactsScreen} 
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-export default App;
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

f2b26360892231d4c26c22f79e5af804aaa122c0
Kara Barrese <karabarrese@gmail.com>
2023-11-11 16:46:54 -0800

    Working on locations

--- Insight/App.js
+++ Insight/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+<<<<<<< Updated upstream
 import { StyleSheet, Text, View } from 'react-native';
 //import {GetLocation} from 'react-native-get-location'
 
@@ -10,6 +11,79 @@ export default function App() {
     </View>
   );
 }
+=======
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+ 
+  url = "https://maps.googleapis.com/maps/api/directions/json";
+  
+  class LatLng {
+    constructor(lat, lon) {
+      this.lat = lat;
+      this.lon = lon;
+    }
+  }
+
+  /*function makeLatLng(lat,lon)
+  {
+    return {
+      "latitude": lat,
+      "longitude": lon
+    };
+  }*/
+  //using this to make a LatLng
+  //const location1 = new LatLng(37,-121);
+  waypoint =
+  {
+    "via": boolean,
+    "vehicleStopover": boolean,
+    "sideOfRoad": boolean,
+  
+    // Union field location_type can be only one of the following:
+    "location": {
+      object (Location)
+    },
+    "placeId": string,
+    "address": string
+    // End of list of possible types for union field location_type.
+  }
+  params = 
+  {}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
+} 
+>>>>>>> Stashed changes
 
 const styles = StyleSheet.create({
   container: {
@@ -18,6 +92,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+<<<<<<< Updated upstream
 });
 
 /*GetLocation.getCurrentPosition({
@@ -31,3 +106,6 @@ const styles = StyleSheet.create({
   const { code, message } = error;
   console.warn(code, message);
 })*/
+=======
+});
+>>>>>>> Stashed changes
